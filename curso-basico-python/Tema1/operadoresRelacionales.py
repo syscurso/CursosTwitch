89d51924c19c3ddc26909492f9b130570179a106
syscurso <syscurso@gmail.com>
2021-02-06 16:49:59 +0100

    OperadoresRelacionales

---
+++ curso-basico-python/Tema1/operadoresRelacionales.py
@@ -0,0 +1,45 @@
+
+
+# Operadores relacionales
+#  < > <= >= == !=
+
+
+#Operador relacional <
+
+numeroUno = 5
+numeroDos = 5
+
+print(numeroUno < numeroDos)
+
+# Operador relacional >
+
+print(numeroUno > numeroDos)
+
+# Operador relacional <=
+
+print(numeroUno <= numeroDos)
+
+# Operador relacional >=
+
+print(numeroUno >= numeroDos)
+
+# Operador relacional ==
+
+print(numeroUno == numeroDos)
+
+# Operador relacional !=
+
+print(numeroUno != numeroDos)
+
+
+
+# --------------------------------
+
+#EJERCICIOS
+
+numeroSeis = '51'
+
+# ↓↓ Descomentar el print para ver resultado ↓↓
+
+# print(numeroSeis == str(51))
+
