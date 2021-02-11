94457794a7663bb30fde50591e32f8fcbca5e03e
syscurso <syscurso@gmail.com>
2021-02-11 11:57:01 +0100

    Ejercicios diccionarios

---
+++ curso-basico-python/Tema2/Ejercicios/diccionarios/ejercicios.py
@@ -0,0 +1,70 @@
+
+# Diccionario del que disponemos para los ejercicios
+diccionario = {'canal' : 'syslasiGB', 'curso' : 'python', 'integrantes' : ['holou', 'da_chuck', 'pastor']}
+
+# EJERCICIO 1
+# Cra una variable que almacene tú nombre de la clave integrantes y printeala (si no está, elige el que quieras)
+
+
+
+# EJERCICIO 2
+# Elimina el canal  :(   Luego printea el diccionario para comprobarlo
+
+
+
+# EJERCICIO 3
+# Crea dos variables, una que contenga todas las llaves y otra que contenga todos los valores. printealas.
+
+
+
+# EJERCICIO 4
+# Utilizando las variables del ejercicio anterior. Crea dos nuevas variables y obten la llave 'curso' y el valor 'python'. Printealo
+
+
+
+# EJERCICIO 5
+# Con las llaves 'moneda' y los valores 'codigo'. Crea una variable que contenga la creación de un diccionario automático
+
+moneda = ('Bitcoin', 'Ether', 'Ripple', 'Litecoin', 'Dogecoin')
+
+codigo = ('BTC', 'ETH', 'XRP', 'LTC', 'DOGE')
+
+
+
+
+# UNA VEZ AQUÍ. ASEGURATE DE QUE TODOS LOS PRINTS ANTERIORES ESTAN COMENTADOS... ¡COMENZAMOS!
+# EJERCICIO EXTRA --VENCER AL MONSTRUO--
+# Tenemos dos diccionarios. Uno nuestro personaje y otro el monstruo.
+
+personaje = {'nombre' : 'GameStop', 'fuerza' : 5, 'habilidades' : ['Reddit', 'Comunidad', 'Inversion']}
+
+monstruo = {'nombre' : 'WallStreet', 'vida' : 5, 'habilidades' : ['Apuesta', 'InversionSegura', 'Cagada']}
+
+
+# En este momento. El monstruo realiza su ataque Cagada
+# Crea una variable que contenga la habilidad cagada y printeala
+
+
+
+# Al detectar el ataque de WallStreet. La comunidad se pone manos a la obra.
+# De esta manera, obtienen +1 de fuerza. (Haz que el valor de fuerza de tú personaje suba 1)
+
+
+
+# Printea la fuerza de tu personaje para comprobar que ha subido
+
+
+
+# En este momento tú personaje realiza el ataque Reddit
+# Crea una variable que contenga la habilidad Reddit y printeala
+
+
+
+# Crea una variable que compare si la fuerza de tu personaje es MAYOR que la vida del monstruo
+
+
+
+# Printea la variable. En caso de ser True habremos obtenido la VICTORIA!!
+
+
+
--- curso-basico-python/Tema2/diccionarios.py
+++ curso-basico-python/Tema2/diccionarios.py
@@ -3,30 +3,45 @@
 # Su estructura se compone de Llave-Valor  Key-Value
 # La llave no puede estar repetida
 
+#CREAR DICCIONARIO
 diccionario = {'nombre' : 'holou', 'edad' : 22, 'curso' : ['Pygame', 'Tkinter', 'Flask']}
 
+#OBTENER VALOR DE LA CLAVE NOMBRE
 nombre = diccionario["nombre"]
+#OBTENER VALOR DE LA CLAVE CURSO
 curso = diccionario["curso"]
 
+#VARIABLE ALFABETO
 letras = 'abcdefghijklmnñopqrstuvwxyz'
+
+#VARIABLE POSICIONES ALFABETO
 posiciones = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
 
+#CREAR UN DICCIONARIO DEL ALFABETO
 diccionarioAutomatico = dict(zip(letras, posiciones))
 
-
+#OBTENER VALOR DE LA CLAVE NOMBRE
 valor = diccionario.get('nombre')
+
+#ELIMINAR clave-valor de EDAD
 valor1 = diccionario.pop('edad')
 
-
+#OBTENER TODAS LAS CLAVES DEL DICCIONARIO (EN OBJETO)
 llaves = diccionario.keys()
+
+#CONVERTIR EL OBJETO A LISTA PARA SU MANEJO
 llavesLista = list(llaves)
 
+#OBTENER TODOS LOS VALORES DEL DICCIONARIO (EN OBJETO)
 valores = diccionario.values()
+
+#CONVERTIR EL OBJETO A LISTA PARA SU MANEJO
 valoresLista = list(valores)
 
-
+#COPIAR EL DICCIONARIO EN UNA VARIABLE NUEVA
 copiaDiccionario = diccionario.copy()
 
+#LIMPIAR EL DICCIONARIO PRINCIPAL
 diccionario.clear()
 
 # EJERCFICIO 
